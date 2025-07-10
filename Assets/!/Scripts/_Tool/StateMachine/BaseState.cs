b50c223da4fa5e53d35e11b411af477a2a9960e3
Dan_Lang_A <long1862004@gmail.com>
2025-07-10 15:30:41 +0700

    Init project

---
+++ Assets/!/Scripts/_Tool/StateMachine/BaseState.cs
@@ -0,0 +1,29 @@
+﻿// Author: DanlangA
+
+using System;
+using UnityEngine;
+using System.Collections;
+
+namespace DanLangA
+{
+    public abstract class BaseState<EState> where EState : Enum
+    {
+        protected BaseState(EState key)
+        {
+            StateKey = key;
+        }
+
+        public          EState      StateKey { get; private set; }
+        public abstract void        EnterState();
+        public abstract void        UpdateState();
+        public abstract IEnumerator ExitState();
+        public abstract EState      GetNextState();
+        public abstract void        OnTriggerEnter(Collider    other);
+        public abstract void        OnTriggerStay(Collider     other);
+        public abstract void        OnTriggerExit(Collider     other);
+        public abstract void        OnCollisionEnter(Collision collision);
+        public abstract void        OnCollisionStay(Collision  collision);
+        public abstract void        OnCollisionExit(Collision  collision);
+        public abstract void        OnMouseDown();
+    }
+}
