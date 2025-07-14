f04c771bf37ff7bebcb92241efa89b671a703676
Dan_Lang_A <long1862004@gmail.com>
2025-07-14 13:08:06 +0700

    Update Joystick

--- Assets/!/Scripts/Player/PlayerMovement.cs
+++ Assets/!/Scripts/Player/PlayerMovement.cs
@@ -3,32 +3,125 @@
 using System;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 namespace _.Scripts.Player
 {
     [RequireComponent(typeof(NavMeshAgent))]
     public class PlayerMovement : MonoBehaviour
     {
-        [HideInInspector]
-        public NavMeshAgent agent;
-
-        private RaycastHit[] hits = new RaycastHit[1];
+        private static readonly  int      IsWalking = Animator.StringToHash("IsWalking");
+        [SerializeField] private Animator animator;
+        
+        [Header("Navigation Movement")]
+        [SerializeField] private NavMeshAgent agent;
+        
+        [Header("Joystick Movement")]
+        [SerializeField] private RectTransform joystick;
+        [SerializeField] private RectTransform floatingJoystick;
+        [SerializeField] private RectTransform knob;
+        [SerializeField] private Vector2 joystickSize;
+        private Finger _movementFinger;
+        [SerializeField] private Vector2 movementAmount;
         
         private void Awake()
         {
-            agent = GetComponent<NavMeshAgent>();
+            agent    = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+
+            floatingJoystick = joystick.GetChild(0).GetComponent<RectTransform>();
+            knob             = floatingJoystick.GetChild(0).GetComponent<RectTransform>();
+            joystickSize     = joystick.sizeDelta;
+        }
+
+        private void OnEnable()
+        {
+            EnhancedTouchSupport.Enable();
+            Touch.onFingerDown += OnFingerDown;
+            Touch.onFingerMove += OnFingerMove;
+            Touch.onFingerUp += OnFingerUp;
+        }
+
+        private void OnDisable()
+        {
+            Touch.onFingerDown -= OnFingerDown;
+            Touch.onFingerMove -= OnFingerMove;
+            Touch.onFingerUp -= OnFingerUp;
         }
 
         private void Update()
         {
-            if (Input.GetKeyUp(KeyCode.Mouse0))
+            Vector3 scaleMovement = new Vector3(movementAmount.x, 0f, movementAmount.y) * Time.deltaTime * agent.speed;
+            agent.transform.LookAt(agent.transform.position + scaleMovement, Vector3.up);
+            agent.Move(scaleMovement);
+        }
+
+        private void OnFingerDown(Finger finger)
+        {
+            if (_movementFinger == null && finger.screenPosition.x < Screen.width / 2f)
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.RaycastNonAlloc(ray, hits) > 0)
-                {
-                    agent.SetDestination(hits[0].point);
-                }
+                _movementFinger = finger;
+                movementAmount = Vector2.zero;
+                joystick.gameObject.SetActive(true);
+                joystick.anchoredPosition = ClampStartPosition(finger.screenPosition);
             }
         }
+
+        private Vector2 ClampStartPosition(Vector2 screenPosition)
+        {
+            screenPosition.x = screenPosition.x > joystickSize.x / 2f ? screenPosition.x : joystickSize.x / 2f;
+            screenPosition.y = screenPosition.y > joystickSize.y / 2f ? (screenPosition.y > Screen.height - joystickSize.y / 2f ? Screen.height - joystickSize.y / 2f : screenPosition.y) : joystickSize.y / 2f;
+            return screenPosition;
+        }
+
+        private void OnFingerMove(Finger finger)
+        {
+            if (_movementFinger == finger)
+            {
+                float   maxMovement  = joystickSize.x / 2f;
+                Touch   currentTouch = finger.currentTouch;
+                knob.anchoredPosition = Vector2.Distance(currentTouch.screenPosition, joystick.anchoredPosition) > maxMovement ? (currentTouch.screenPosition - joystick.anchoredPosition).normalized * maxMovement : currentTouch.screenPosition - joystick.anchoredPosition;
+                movementAmount = knob.anchoredPosition / maxMovement;
+                animator.SetBool(id:IsWalking, true);
+            }
+        }
+
+        private void OnFingerUp(Finger finger)
+        {
+            if (_movementFinger == finger)
+            {
+                _movementFinger = null;
+                knob.anchoredPosition = Vector2.zero;
+                movementAmount = Vector2.zero;
+                joystick.gameObject.SetActive(false);
+                animator.SetBool(id:IsWalking, false);
+            }
+        }
+
+        // private void OnGUI()
+        // {
+        //     GUIStyle style = new GUIStyle()
+        //                      {
+        //                          fontSize  = 15,
+        //                          fontStyle = FontStyle.Bold,
+        //                          normal = new GUIStyleState()
+        //                                   {
+        //                                       textColor = Color.red
+        //                                   }
+        //                      };
+        //     if (_movementFinger != null)
+        //     {
+        //         GUI.Label(new Rect(10, 35, 500, 20), $"Finger Start Position: {_movementFinger.currentTouch.startScreenPosition}", style);
+        //         GUI.Label(new Rect(10, 65, 500, 20), $"Finger Current Position: {_movementFinger.currentTouch.screenPosition}", style);
+        //     }
+        //     else
+        //     {
+        //         GUI.Label(new Rect(10, 35, 500, 20), "No Current Movement Touch", style);
+        //     }
+        //
+        //     GUI.Label(new Rect(10, 10, 500, 20), $"Screen Size ({Screen.width}, {Screen.height})", style);
+        // }
     }
 }
